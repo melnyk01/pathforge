dd3863c25d0b902cd5a84ffb71b8173c20950447
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-06-22 22:13:35 +0200

    Add local storing for previous records Hard coded for only 3 habits now, will fix in next updates

--- javascropt.js
+++ javascropt.js
@@ -1,3 +1,4 @@
+
  let habits = []
  let completedHabits = {
 
@@ -6,18 +7,49 @@
  let habitRun = document.querySelector('input#run');
  let habitStudy = document.querySelector('input#study');
  let habitRead = document.querySelector('input#read');
+ let dayResult = document.querySelector('#result');
 
 habits.push(habitRun, habitStudy, habitRead);
 
+let history = [ 
+
+]
+
+// Saving the day
 let save = document.querySelector('#save')
 save.addEventListener('click', () => {
+    let today = {
+    }
+    let date = new Date().toISOString().split('.')[0];
+    today.date = date;
     for (let habit of habits) {
         if (habit.checked) {
-            completedHabits[habit.id] = true
+            completedHabits[habit.id] = 'Done!'
         } else {
-            completedHabits[habit.id] = false
+            completedHabits[habit.id] = 'Failed!'
         }
+        today[habit.id] = completedHabits[habit.id];
     }
-
+    history.push(today);
+    localStorage.setItem('history', JSON.stringify(history));
     alert('Day has been successfully saved!');
 })
+// Get history data from local storage and printing it
+history = JSON.parse(localStorage.getItem('history') || {});
+console.table(JSON.parse(localStorage.getItem('history')));
+console.log(history.reverse());
+for (record of history) {
+    let previousRecord = document.createElement('p');
+    dayResult.appendChild(previousRecord);
+    previousRecord.textContent = record.date
+
+    let runResult = document.createElement('div');
+    let studyResult = document.createElement('div');
+    let readResult = document.createElement('div');
+    runResult.textContent = `Run: ${record.run}`
+    studyResult.textContent = `Study: ${record.study}`
+    readResult.textContent = `Read: ${record.read}`
+    dayResult.appendChild(runResult);
+    dayResult.appendChild(studyResult);
+    dayResult.appendChild(readResult);
+}
