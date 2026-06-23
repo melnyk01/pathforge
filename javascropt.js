d2f663cf1c3fec24d2a0c4c994e2b777b358ecfd
melnyk01 <223485602+melnyk01@users.noreply.github.com>
2026-06-23 20:05:45 +0200

    Add localStorage persistence with reset func. Persist habit creation, history, and reset actions in localStorage

--- javascropt.js
+++ javascropt.js
@@ -1,60 +1,94 @@
-let habits = [
-
-]
-let history = [
-    
-]
+let habits = JSON.parse(localStorage.getItem('habits')) || [];
+let history = JSON.parse(localStorage.getItem('history')) || [];
 let habitList = document.querySelector('#habitList');
-let habitRun = document.querySelector('input#run');
 let dayResult = document.querySelector('#result');
-habits.push(habitRun);
+let sessionHabits = [];
+
+//Clear records button
+
+let clearRecordsButton = document.querySelector('#clear-records')
+clearRecordsButton.addEventListener('click', () => {
+    localStorage.removeItem('history');
+    while (dayResult.lastChild) {
+        dayResult.removeChild(dayResult.lastChild);
+    }
+    alert('History was successfully cleared!')
+});
+
+// Clear habits button
+
+let clearHabitsButton = document.querySelector('#clear-habits')
+clearHabitsButton.addEventListener('click', () => {
+    localStorage.removeItem('habits');
+    while (habitList.lastChild && (habitList.lastChild.id != 'createHabit')) {
+        habitList.removeChild(habitList.lastChild);
+    }
+    alert('Habits were successfully cleared!')
+});
+
 
 //Creating a new habit
 
 let createHabit = document.querySelector('#createHabit')
 createHabit.addEventListener('click', () => {
+    const newHabitId = prompt('What habit would you like to track?');
     const newHabit = document.createElement('li');
     const label = document.createElement('label');
     const input = document.createElement('input');
-    input.type = 'checkbox'
+    input.id = newHabitId;
+    input.type = 'checkbox';
     habitList.appendChild(newHabit);
     newHabit.appendChild(label);
     label.appendChild(input);
-    label.appendChild(document.createTextNode(prompt('What habit would you like to track?')));
+    label.appendChild(document.createTextNode(newHabitId));
+    sessionHabits.push(input);
+    
 })
-
-//Clear button
-
-let clearButton = document.querySelector('#clear-records')
-clearButton.addEventListener('click', () => {
-    localStorage.removeItem('history');
-    alert('History was successfully cleaned!')
-});
 
 // Saving the day
 
 let save = document.querySelector('#save');
 save.addEventListener('click', () => {
+
     let today = {
     }
     let date = new Date().toISOString().split('.')[0];
     today.date = date;
-    for (let habit of habits) {
-        if (habit.checked) {
-            today[habit.id] = 'Done!'
+    for (let habit of sessionHabits) {
+        let habitData = {
+            id: habit.id,
+            checked: habit.checked,
+        };
+        habits.push(habitData);
+        if (habitData.checked) {
+            today[habitData.id] = 'Done!'
+            habitData.checked = false
         } else {
-            today[habit.id] = 'Failed!'
+            today[habitData.id] = 'Failed!'
         }
     }
     history.push(today);
     localStorage.setItem('history', JSON.stringify(history));
+    localStorage.setItem('habits', JSON.stringify(habits));
     alert('Day has been successfully saved!');
 })
-// Get history data from local storage and printing it
 
-history = JSON.parse(localStorage.getItem('history')) || [];
+// Display old habits
+if (habits.length > 0) {
+    for (habit of habits) {
+        const newHabit = document.createElement('li');
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        input.id = habit.id;
+        input.type = 'checkbox';
+        habitList.appendChild(newHabit);
+        newHabit.appendChild(label);
+        label.appendChild(input);
+        label.appendChild(document.createTextNode(habit.id));
+    }    
+}
 
-// Reversed to get a new data first
+// Prints records; Reversed to get a new data first
 
 if (history.length > 0) {
     for (record of history.reverse()) { 
